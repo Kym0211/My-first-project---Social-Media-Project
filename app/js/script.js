30c58e6b58424d0556f6a19bc0929f3c9c151d99
Kavyam <kavyam.ksingh@gmail.com>
2024-03-30 21:10:13 +0530

    new file:   .gitattributes 	new file:   .gitignore 	new file:   README-template.md 	new file:   README.md 	new file:   app/js/script.js 	new file:   app/scss/components/card-grid.scss 	new file:   app/scss/components/card.scss 	new file:   app/scss/components/header.scss 	new file:   app/scss/components/index.scss 	new file:   app/scss/components/toggle.scss 	new file:   app/scss/globals/boilerplates.scss 	new file:   app/scss/globals/colors.scss 	new file:   app/scss/globals/fonts.scss 	new file:   app/scss/globals/index.scss 	new file:   app/scss/globals/layout.scss 	new file:   app/scss/globals/typography.scss 	new file:   app/scss/style.scss 	new file:   app/scss/util/breakpoints.scss 	new file:   app/scss/util/functions.scss 	new file:   app/scss/util/index.scss 	new file:   design/active-states-dark.jpg 	new file:   design/active-states-light.jpg 	new file:   design/desktop-design-dark.jpg 	new file:   design/desktop-design-light.jpg 	new file:   design/desktop-preview.jpg 	new file:   design/mobile-design-dark.jpg 	new file:   design/mobile-design-light.jpg 	new file:   dist/script.js 	new file:   dist/script.js.map 	new file:   dist/style.css 	new file:   dist/style.css.map 	new file:   gulpfile.js 	new file:   images/favicon-32x32.png 	new file:   images/icon-down.svg 	new file:   images/icon-facebook.svg 	new file:   images/icon-instagram.svg 	new file:   images/icon-twitter.svg 	new file:   images/icon-up.svg 	new file:   images/icon-youtube.svg 	new file:   index.html 	new file:   notes.md 	new file:   package-lock.json 	new file:   package.json 	new file:   style-guide.md

---
+++ app/js/script.js
@@ -0,0 +1,96 @@
+// const darkMode = document.getElementById('dark');
+// const lightMode = document.getElementById('light');
+// const body = document.querySelector('body');
+// let currentTheme = localStorage.getItem('theme');
+
+// // console.log(currentToggle);
+// darkMode.addEventListener('click', () => {
+//     if (body.classList.contains('light')) {
+//         body.classList.remove('light');
+//     };
+//     if (currentTheme != 'dark') {
+//         localStorage.setItem('theme', 'dark');
+//         currentTheme = 'dark';
+//     }
+//     body.classList.add(currentTheme);
+// });
+
+// lightMode.addEventListener('click', () => {
+//     console.log('light');
+//     if (body.classList.contains('dark')) {
+//         body.classList.remove('dark');
+//     };
+
+//     if (currentTheme != 'light') {
+//         localStorage.setItem('theme', 'light');
+//         currentTheme = 'light';
+//     }
+//     body.classList.add(currentTheme);
+// });
+
+
+// body.classList.add(currentTheme);
+// document.getElementById(currentTheme).checked = true;
+
+
+
+//=====================
+
+
+/* 
+The first time the page is loaded, the color mode set on the preference 
+is used and set as 'default' in the local storage. 
+Changing the default preferences works the same way as changing the 
+color mode using the buttons, if the page is loaded.
+When the page is reloaded, whatever is the value set on the local storage
+has precedence over the values in the preference. If the preference
+changed after the page was visited - and the page is not loaded - 
+the last value saved on the local storage is loaded. 
+*/
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+
+const setDarkMode = () => {
+  document.querySelector('body').classList = 'dark';
+  localStorage.setItem('colorMode', 'dark');
+};
+
+const setLightMode = () => {
+  document.querySelector('body').classList = 'light';
+  localStorage.setItem('colorMode', 'light');
+};
+
+const colorModeFromLocalStorage = () => {
+  return localStorage.getItem('colorMode');
+};
+
+const colorModeFromPreferences = () => {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches 
+              ? 'dark'
+              : 'light' // If preference is set or does not match anything (light is default)
+};
+
+const loadAndUpdateColor = () => {
+  // local storage has precendence over the prefers-color-scheme
+  const color = colorModeFromLocalStorage() || colorModeFromPreferences();
+  color == 'dark' ? darkButton.click() : lightButton.click();
+};
+
+// when the inputs are clicked, check which radio button is checked and change the color
+const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+radioButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    darkButton.checked ? setDarkMode() : setLightMode();
+  });
+});
+
+// when the prefers-color-scheme changes, this event will be emitted
+// event reflects the media query, if it matches, the new color is dark, else it is light
+window.matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', (event) => {
+        event.matches ? darkButton.click() : lightButton.click();
+      });
+      
+// Load the right color on startup - localStorage has precedence
+loadAndUpdateColor();
---
+++ notes.md
@@ -0,0 +1,14 @@
+# Functional Requirements and Notes
+
+Light/Dark Mode toggle
+- what HTML markup(accessible) -- https://scottaohara.github.io/ally_styled_form_controls/src/radio-button--switch/
+- Switching between light/dark mode via JS
+- Three option toggle: light/dark/system pref -- https://codepen.io/renddrew/pen/bRomb?editors=1100
+
+CSS variables (custom properties)
+
+Prefers-color-scheme media query
+
+Accessibility
+- Use correct heading tags
+- Screenreader-only text for card titles/usernames
